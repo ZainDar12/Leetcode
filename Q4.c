d4a2048c083cead973c21ed4f44b1bd8d94b79ff
ZainDar12 <k250146@nu.edu.pk>
2025-11-19 16:54:51 +0500

    Q4.c
    Implement function to move all zeroes to the end of the array.

---
+++ Q4.c
@@ -0,0 +1,12 @@
+//Q4.c
+void moveZeroes(int* nums, int numsSize) {
+    int insertPos = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[insertPos++] = nums[i];
+        }
+    }
+    while (insertPos < numsSize) {
+        nums[insertPos++] = 0;
+    }
+}
